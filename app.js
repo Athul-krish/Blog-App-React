3d3c0ca6a952a291b96ab900c7be5a4bc6bb30e7
Athul-krish <athulkrishnapanamakkal@gmail.com>
2026-07-16 00:48:49 +0530

    signIn API completed

--- app.js
+++ app.js
@@ -15,6 +15,39 @@ Mongoose.connect(
   "mongodb://athulkrishnapanamakkal:athul123@ac-pikpijl-shard-00-00.wjtqgxj.mongodb.net:27017,ac-pikpijl-shard-00-01.wjtqgxj.mongodb.net:27017,ac-pikpijl-shard-00-02.wjtqgxj.mongodb.net:27017/blogAppDb?ssl=true&replicaSet=atlas-a3e10y-shard-0&authSource=admin&appName=Cluster0",
 );
 
+
+
+//signin
+app.post("/signIn", async (req, res) => {
+    let input = req.body;
+    let result = userModel.find({ email: req.body.email }).then(
+        (items)=>{
+            if(items.length>0){
+                const passwordvalidator = Bcrypt.compareSync(req.body.password, items[0].password);
+                if(passwordvalidator){
+                  jwt.sign({email:req.body.email},"blogApp",{expiresIn:"1d"},
+                    (err,token)=>{
+                      if(err){
+                        res.json({"Status":"error","errorMessage":err})
+                        }else{
+                            res.json({"Status":"success","token":token,"userId":items[0]._id})
+                      }
+                    })
+                }  else{
+                    res.json({"Status":"Invalid password"})
+                }
+
+
+        }else{
+            res.json({"Status":"Invalid email-id"})
+        }
+})
+
+});
+
+
+
+//signup
 app.post("/signup", async (req, res) => {
   let input = req.body;
   let hashedPassword = Bcrypt.hashSync(req.body.password, 10);
